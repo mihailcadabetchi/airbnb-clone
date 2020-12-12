d615c2327bec7c331a89a747c6074336fef89efc
Mihail Cadabetchi <74629946+mihailcadabetchi@users.noreply.github.com>
2020-12-12 14:20:32 +0100

    airbnb-clone
    airbnb-clone

---
+++ precache-manifest.0672081597508a3b7fa92ab06e7f9f75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61998ad755b7c7310ba98a32aacc45b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f928879a3c31c32a80c",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "59317734a0aac499356a",
+    "url": "/static/css/main.3b7c784c.chunk.css"
+  },
+  {
+    "revision": "6f928879a3c31c32a80c",
+    "url": "/static/js/2.63c79cae.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.63c79cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59317734a0aac499356a",
+    "url": "/static/js/main.2aaaf410.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
